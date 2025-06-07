9e9a24714928a2276fdbf770fac6c8853165886e
svikas641 <svikas641@gmail.com>
2025-06-08 00:26:23 +0530

    Remove basePath and assetPrefix for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/pokedex-nextjs' : '',
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
